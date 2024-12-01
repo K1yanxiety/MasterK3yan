f60b83b1fd78a5d2f873751c18dfecabcb01910f
Keane Jon Ambil <keanejon.ambil@carsu.edu.ph>
2024-12-01 23:15:00 +0800

    dwdw
    weew

---
+++ script.js
@@ -0,0 +1,175 @@
+window.addEventListener("scroll", function() {
+  var header = document.querySelector('.text-1');
+  var headerPosition = header.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 1;
+
+  if (headerPosition < screenPosition) {
+    header.classList.add('show');
+  }
+});
+
+  
+
+
+
+
+
+
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
+  var count_particles, stats, update;
+  stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
+  
+  count_particles = document.querySelector('.js-count-particles');
+  
+  update = function() {
+    stats.begin();
+    stats.end();
+    
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    
+    requestAnimationFrame(update);
+  };
+  
+  requestAnimationFrame(update);
+  
+
+
+
+  function showImage() {
+    // Show the modal overlay and apply blur to only the content-wrapper
+    document.getElementById("modal-overlay").style.display = "flex";
+    document.querySelector(".content-wrapper").style.filter = "blur(5px)";
+    
+    // Disable scrolling
+    document.body.style.overflow = "hidden";
+}
+
+function hideImage() {
+    // Hide the modal overlay and remove blur from content-wrapper
+    document.getElementById("modal-overlay").style.display = "none";
+    document.querySelector(".content-wrapper").style.filter = "none";
+    
+    // Enable scrolling again
+    document.body.style.overflow = "auto";
+}
+
+
+// for popup image
+
